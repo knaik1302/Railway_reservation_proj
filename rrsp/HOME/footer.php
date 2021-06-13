476962c95578657a9c0ed0e7fc3f128686a99579
Kartik V Naik <85819372+knaik1302@users.noreply.github.com>
2021-06-13 12:53:09 +0530

    Update footer.php

--- rrsp/HOME/footer.php
+++ rrsp/HOME/footer.php
@@ -2,6 +2,6 @@
 <!DOCTYPE html>
 <html>
     <body>
-    <footer style="color: aliceblue;background-color: crimson;margin-top:400px; padding:0.05px;"><p style="margin-left: 20px;">Created by: Kartik V Naik and Hawaralu Vignesh</p> </footer>
+    <footer style="color: aliceblue;background-color: crimson;margin-top:400px; padding:0.05px;"><p style="margin-left: 20px;">Created by: Kartik V Naik and My partner</p> </footer>
     </body>
 </html>
